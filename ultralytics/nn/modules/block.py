11d8ad13abc9a6eca01540ddc08c0e31a9a4b95d
cineaj <cinefxmedialtd@gmail.com>
2024-10-14 10:08:37 +0300

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -16,6 +16,7 @@ __all__ = (
     "HGStem",
     "SPP",
     "SPPF",
+    "SPPF_UniRepLK",
     "C1",
     "C2",
     "C3",
@@ -187,7 +188,24 @@ class SPPF(nn.Module):
         y.extend(self.m(y[-1]) for _ in range(3))
         return self.cv2(torch.cat(y, 1))
 
+class SPPF_UniRepLK(nn.Module):
+    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher."""
 
+    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
+        super().__init__()
+        c_ = c1 // 2  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_ * 4, c2, 1, 1)
+        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
+        self.UniRepLK = UniRepLKNetBlock(c_ * 4, kernel_size=k)
+
+    def forward(self, x):
+        """Forward pass through Ghost Convolution block."""
+        x = self.cv1(x)
+        y1 = self.m(x)
+        y2 = self.m(y1)
+        return self.cv2(self.UniRepLK(torch.cat((x, y1, y2, self.m(y2)), 1)))
+        
 class C1(nn.Module):
     """CSP Bottleneck with 1 convolution."""
 
