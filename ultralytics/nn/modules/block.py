5c2922537395cb5cca207542ad1e89fb087a5baf
cineaj <cinefxmedialtd@gmail.com>
2024-10-14 19:51:25 +0300

    Update block.py

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -7,7 +7,7 @@ import torch.nn.functional as F
 
 from ultralytics.utils.torch_utils import fuse_conv_and_bn
 
-from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad, UniRepLKNetBlock
+from .conv import Conv, DWConv, GhostConv, LightConv, RepConv, autopad, UniRepLKNetBlock,SimConv
 from .transformer import TransformerBlock
 
 __all__ = (
@@ -18,6 +18,7 @@ __all__ = (
     "SPPF",
     "SPPF_UniRepLK",
     "C1",
+    "SimSPPCSPC",
     "C2",
     "C3",
     "C2f",
@@ -242,33 +243,6 @@ class SPPCSPC(nn.Module):
         y1 = self.cv6(self.cv5(torch.cat([x1] + [m(x1) for m in self.m], 1)))
         y2 = self.cv2(x)
         return self.cv7(torch.cat((y1, y2), dim=1))
-
-
-class SimConv(nn.Module):
-    """Normal Conv with ReLU activation"""
-
-    def __init__(
-        self, in_channels, out_channels, kernel_size, stride, groups=1, bias=False
-    ):
-        super().__init__()
-        padding = kernel_size // 2
-        self.conv = nn.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size=kernel_size,
-            stride=stride,
-            padding=padding,
-            groups=groups,
-            bias=bias,
-        )
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.act = nn.LeakyReLU()
-
-    def forward(self, x):
-        return self.act(self.bn(self.conv(x)))
-
-    def forward_fuse(self, x):
-        return self.act(self.conv(x))
 
 
 class SimSPPFCSPC(nn.Module):
