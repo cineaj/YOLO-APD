158d1202c8fa0d2423401488be8e25e53af0f7fe
cineaj <cinefxmedialtd@gmail.com>
2024-10-14 10:07:20 +0300

    Update conv.py

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -361,23 +361,7 @@ class UniRepLKNetBlock(nn.Module):
             self.pwconv2 = nn.Sequential(new_linear, self.pwconv2[1])
 
 
-class SPPF_UniRepLK(nn.Module):
-    """Spatial Pyramid Pooling - Fast (SPPF) layer for YOLOv5 by Glenn Jocher."""
 
-    def __init__(self, c1, c2, k=5):  # equivalent to SPP(k=(5, 9, 13))
-        super().__init__()
-        c_ = c1 // 2  # hidden channels
-        self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = Conv(c_ * 4, c2, 1, 1)
-        self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
-        self.UniRepLK = UniRepLKNetBlock(c_ * 4, kernel_size=k)
-
-    def forward(self, x):
-        """Forward pass through Ghost Convolution block."""
-        x = self.cv1(x)
-        y1 = self.m(x)
-        y2 = self.m(y1)
-        return self.cv2(self.UniRepLK(torch.cat((x, y1, y2, self.m(y2)), 1)))
 #---------------------------------------------------------mxintro----------------------------------------------
 
 
